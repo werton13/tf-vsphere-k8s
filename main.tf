df2282847d3e84d9fc8ec57eae121fabda775196
Popov Sergey <sipopov2@mts.ru>
2023-01-27 17:42:59 +0300

    commit refactoring

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
 data "vsphere_datacenter" "datacenter" {
   name = "${var.dcname}"
 }
@@ -32,22 +39,102 @@ data "vsphere_virtual_machine" "template_from_ovf" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "template_file" "cloud-init" {
+data "template_file" "cloudinit_master_node" {
 #  template = file("./templates/userdata.yaml") 
-template = file("${path.module}/templates/userdata.yaml")
+template = file("${path.module}/templates/userdata_m.yaml")
   vars = {
-    vm_user_name = var.vm_user_name
-    vm_user_displayname = var.vm_user_displayname
-    vm_user_password  = var.vm_user_password
-    vm_user_ssh_key   = var.vm_user_ssh_key
-    ansible_repo_url  = var.ansible_repo_url
-    ansible_playbook  = var.ansible_playbook
-    vm_ssh_port       = var.vm_ssh_port
-    ansible_repo_name = var.ansible_repo_name
-    esxi_host_name    = var.esxi_host_name
-    vsphere_host_ip   = var.vsphere_host_ip
-    vnc_password      = var.vnc_password
+
+    vsphere_server      = var.vsphere_server
+    vsphere_host_ip     = var.vsphere_host_ip
+    vsphere_user        = var.vsphere_user
+    vsphere_password    = var.vsphere_password
+    dcname              = var.dcname
+    dcstore_name        = var.dcstore_name
+    esxi_host_name      = var.esxi_host_name
+    vsphere_clustername = var.vsphere_clustername
+
+    vm_user_name        = var.vm_user_name
+    vm_user_password    = var.vm_user_password
+    vm_user_displayname = var.vm_user_displayname     
+    vm_user_ssh_key     = var.vm_user_ssh_key
+    vm_user_ssh_pk      = var.vm_user_ssh_pk
+
+    ansible_repo_url    = var.ansible_repo_url
+    ansible_repo_name   = var.ansible_repo_name
+    ansible_playbook    = var.ansible_playbook
+    
+    os_admin_username   = var.os_admin_username
+    os_nic1_name        = var.os_nic1_name
+
+    k8s_ver             = var.k8s_ver
+    k8s_version_short   = var.k8s_version_short
+    calico_version      = var.calico_version
+    vsphere_csi_driver_version = var.vsphere_csi_driver_version
+    k8s_controlPlane_Endpoint = var.k8s_controlPlane_Endpoint
+    k8s_service_subnet  = var.k8s_service_subnet
+    k8s_pod_subnet      = var.k8s_pod_subnet
+    calico_network_cidr_blocksize = var.calico_network_cidr_blocksize
+    k8s_cluster_id      = var.k8s_cluster_id
+    sc_storage_policy_name = var.sc_storage_policy_name
+    sc_name             = var.sc_name
+    hosts_entry1        = "${var.vms.masters.ip_pool[0]}  ${var.vms.masters.pref-0}"
+    hosts_entry2        = "${var.vms.masters.ip_pool[1]}  ${var.vms.masters.pref-1}"
+    hosts_entry3        = "${var.vms.workers.ip_pool[0]}  ${var.vms.workers.pref-0}"
+    hosts_entry4        = "${var.vms.workers.ip_pool[1]}  ${var.vms.workers.pref-1}"
+    hosts_entry5        = "${var.vms.workers.ip_pool[2]}  ${var.vms.workers.pref-2}"
+    master0_ip          =  "${var.vms.masters.ip_pool[0]}"
+    master1_ip          =  "${var.vms.masters.ip_pool[1]}"
+    worker0_ip          =  "${var.vms.workers.ip_pool[0]}" 
+    worker1_ip          =  "${var.vms.workers.ip_pool[1]}"
+    worker2_ip          =  "${var.vms.workers.ip_pool[2]}"
+    
   }
+}
+data "template_file" "cloudinit_worker_node" {
+#  template = file("./templates/userdata.yaml") 
+template = file("${path.module}/templates/userdata_w.yaml")
+  vars = {
+
+    vsphere_server      = var.vsphere_server
+    vsphere_host_ip     = var.vsphere_host_ip
+    vsphere_user        = var.vsphere_user
+    vsphere_password    = var.vsphere_password
+    dcname              = var.dcname
+    dcstore_name        = var.dcstore_name
+    esxi_host_name      = var.esxi_host_name
+    vsphere_clustername = var.vsphere_clustername
+
+    vm_user_name        = var.vm_user_name
+    vm_user_password    = var.vm_user_password
+    vm_user_displayname = var.vm_user_displayname     
+    vm_user_ssh_key     = var.vm_user_ssh_key
+
+    ansible_repo_url    = var.ansible_repo_url
+    ansible_repo_name   = var.ansible_repo_name
+    ansible_playbook    = var.ansible_playbook
+    k8s_ver             = var.k8s_ver
+    k8s_version_short   = var.k8s_version_short
+    calico_version      = var.calico_version
+    vsphere_csi_driver_version = var.vsphere_csi_driver_version
+    k8s_controlPlane_Endpoint = var.k8s_controlPlane_Endpoint
+    k8s_service_subnet  = var.k8s_service_subnet
+    k8s_pod_subnet      = var.k8s_pod_subnet
+    calico_network_cidr_blocksize = var.calico_network_cidr_blocksize
+    k8s_cluster_id      = var.k8s_cluster_id
+    sc_storage_policy_name = var.sc_storage_policy_name
+    sc_name             = var.sc_name
+  }
+}
+  ##experiment here
+data "template_file" "metadata" {
+#  template = file("./templates/userdata.yaml") 
+template = file("${path.module}/templates/metadata.yaml")
+  vars = {
+    ipv4_gateway = var.vm_ipv4_gw
+    dns_server   = var.vm_dns_server
+  }
+
+  #end of expiriment
 
   #}
   
@@ -63,42 +150,134 @@ data "template_cloudinit_config" "cloud-init" {
 }
 
  
-resource "vsphere_virtual_machine" "vmFromLocalOvf"  {
-  name             = var.vm_name
+resource "vsphere_virtual_machine" "k8s_masters_vm"  {
+  name             = "${var.vms.masters.pref}-${count.index}"
+  #"${var.vms.masters.pref}-${var.vms.masters.ip_pool[count.index]}
+  count            = var.vms.masters.vm_count
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   host_system_id   = data.vsphere_host.host.id
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = var.vm_cpu_count
-  memory           = var.vm_ram_size
+  num_cpus         = var.vms.masters.vm_cpu_count
+  memory           = var.vms.masters.vm_ram_size
   guest_id         = "ubuntu64Guest"
+  folder           = var.vsphere_vm_folder
   network_interface {
     network_id = data.vsphere_network.network.id
- 
   }
+
   disk {
     label = "disk0"
-    size  = var.vm_disk_size
+    size  = var.vms.masters.vm_disk_size
   }
-    cdrom {
+
+  cdrom {
     client_device = true
   }
+  
   clone {
      template_uuid = data.vsphere_virtual_machine.template_from_ovf.id
 
   }
  
   vapp {
-  properties = {
-                     
-        }
+  properties = {  }
   }
+
   extra_config = {
-     "guestinfo.userdata" = base64encode(data.template_file.cloud-init.rendered)
+     "guestinfo.userdata" = base64encode(data.template_file.cloudinit_master_node.rendered)
      "guestinfo.userdata.encoding" = "base64"
-     "guestinfo.metadata" = base64encode(file("${path.module}/templates/metadata.yaml"))
-     "guestinfo.metadata.encoding" = "base64" 
+     "guestinfo.metadata" = base64encode(replace((replace((data.template_file.metadata.rendered), "vm_ip_address",var.vms.masters.ip_pool[count.index])), "os_host_name", "${var.vms.masters.pref}-${count.index}"))
+     "guestinfo.metadata.encoding" = "base64"
+    
   }  
 
 }
 
-#out
+resource "vsphere_virtual_machine" "k8s_workers_vm"  {
+  name             = "${var.vms.workers.pref}-${count.index}"
+  #"${var.vms.masters.pref}-${var.vms.masters.ip_pool[count.index]}
+  count            = var.vms.workers.vm_count
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  host_system_id   = data.vsphere_host.host.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = var.vms.workers.vm_cpu_count
+  memory           = var.vms.workers.vm_ram_size
+  guest_id         = "ubuntu64Guest"
+  folder           = var.vsphere_vm_folder
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = var.vms.workers.vm_disk_size
+  }
+
+  cdrom {
+    client_device = true
+  }
+  
+  clone {
+     template_uuid = data.vsphere_virtual_machine.template_from_ovf.id
+
+  }
+ 
+  vapp {
+  properties = {  }
+  }
+
+  extra_config = {
+     "guestinfo.userdata" = base64encode(data.template_file.cloudinit_worker_node.rendered)
+     "guestinfo.userdata.encoding" = "base64"
+     "guestinfo.metadata" = base64encode(replace((replace((data.template_file.metadata.rendered), "vm_ip_address",var.vms.workers.ip_pool[count.index])), "os_host_name", "${var.vms.workers.pref}-${count.index}"))
+     "guestinfo.metadata.encoding" = "base64"
+    
+  }  
+
+}
+
+
+
+#resource "vsphere_virtual_machine" "k8s_workers_vm"  {
+#  name             = "${var.vm_name_pref}-${count.index}"
+#  count            = 3
+#  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+#  host_system_id   = data.vsphere_host.host.id
+#  datastore_id     = data.vsphere_datastore.datastore.id
+#  num_cpus         = var.vm_cpu_count
+#  memory           = var.vm_ram_size
+#  guest_id         = "ubuntu64Guest"
+#  folder           = var.vsphere_vm_folder
+#  network_interface {
+#    network_id = data.vsphere_network.network.id
+#  }
+#
+#  disk {
+#    label = "disk0"
+#    size  = var.vm_disk_size
+#  }
+#
+#  cdrom {
+#    client_device = true
+#  }
+#  
+#  clone {
+#     template_uuid = data.vsphere_virtual_machine.template_from_ovf.id
+#
+#  }
+# 
+#  vapp {
+#  properties = {  }
+#  }
+#
+#  extra_config = {
+#     "guestinfo.userdata" = base64encode(data.template_file.cloud-init.rendered)
+#     "guestinfo.userdata.encoding" = "base64"
+#     "guestinfo.metadata" = base64encode(replace((replace((data.template_file.metadata.rendered), "vm_ip_address",var.vm_ipv4_address[count.index])), "os_host_name", "${var.vm_name_pref}-${count.index}"))
+#     "guestinfo.metadata.encoding" = "base64"
+#    
+#  }  
+#
+#}
+#
+##out
--- variables.tf
+++ variables.tf
@@ -1,112 +1,217 @@
-
-variable vsphere_clustername {
+# vSphere variables 
+variable "vsphere_server" {
   type        = string
   default     = ""
-  description = "vsphere clustername"
-}
+  description = "vsphere_server"
+ }
+variable "vsphere_host_ip"  {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "vsphere_vm_folder" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
 
-variable dcname {
+variable "vsphere_user" {
+  description = ""
+  default = ""
+ }
+variable "vsphere_password" {
+  type        = string
+  default     = ""
+  description = "password for vsphere account"
+ }
+
+variable "dcname" {
   type        = string
   default     = "99Cloud-HUB"
   description = "description"
-}
-variable dcstore_name {
+ }
+variable "dcstore_name" {
   type        = string
   default     = ""
   description = "description"
-}
+ }
 
-variable vsphere_host_ip  {
+variable "esxi_host_name" {
   type        = string
   default     = ""
   description = "description"
-}
-
-variable esxi_host_name {
+ }
+variable "vsphere_clustername" {
   type        = string
   default     = ""
-  description = "description"
-}
-
- variable vm_name {
-   type        = string
-   default     = ""
-   description = "vm name"
+  description = "vsphere clustername"
  }
- variable vm_cpu_count {
-   type        = string
-   default     = "2"
-   description = "quantity of cpu cores for virtual machine"
- }
- variable vm_ram_size {
-   type        = string
-   default     = "1024"
-   description = "Amount of RAM allocated for virtual machine"
- }
- variable vm_disk_size {
-   type        = string
-   default     = "20"
-   description = "system disk size allocated for virtual machine"
- }
- variable vm_template_name  {
+variable "vm_template_name"  {
    type        = string
    default     = ""
    description = "virtual machine template name prepared for cloud-init customisation"
  }
- variable vnet_name {
+
+variable "vnet_name" {
    type        = string
    default     = ""
    description = "virtual network name"
  }
- 
- variable vm_user_name {
+
+variable "vm_user_name" {
    type        = string
    default     = ""
    description = "user name for a new vm user"
  }
-  variable vm_user_displayname {
+variable "vm_user_password" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+
+variable "vm_user_displayname" {
    type        = string
    default     = ""
    description = "user  display name for a new vm user"
  }
- 
-  variable vm_user_password {
-   type        = string
-   default     = ""
-   description = "user password for a new vm user - should be SHA512 hash from desired password"
- }
-  variable vm_user_ssh_key {
+
+variable "vm_user_ssh_key" {
    type        = string
    default     = ""
    description = "user ssh key for a new vm user"
  }
-  variable ansible_repo_url {
+
+variable "vm_ipv4_gw" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "vm_dns_server" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+
+variable "vms" {
+    type = map(object({
+        pref = string
+        vm_cpu_count = string
+        vm_ram_size  = string
+        vm_disk_size = string
+        vm_count = string
+        ip_pool = list(string)
+    }))
+ }
+
+############ for Ansible playbook
+variable "ansible_repo_url" {
    type        = string
    default     = "https://github.com/werton13/Ansible-DSKLinuxCustom.git"
    description = "ansible playbook URL for vm advanced customisations"
  }
- 
- variable  ansible_playbook{
+variable "ansible_repo_name"  {
+  type        = string
+  default     = "Ansible-DSKLinuxCustom"
+  description = "ansible git repository name for vm advanced customisations"
+ }
+variable "ansible_playbook" {
    type        = string
    default     = "main.yaml"
    description = "ansible playbook for vm advanced customisations"
  }
-variable vm_ssh_port {
-  type        = string
-  default     = "22"
-  description = "vm ssh port to open - can be custom, default 22"
-}
-variable ansible_repo_name  {
-  type        = string
-  default     = "Ansible-DSKLinuxCustom"
-  description = "ansible git repository name for vm advanced customisations"
-}
-variable vnc_password {
+variable "os_admin_username" {
   type        = string
   default     = ""
-  description = "password for connecting to a VNC server - it is different from vm user password"
+  description = "description"
+ }
+variable "os_nic1_name" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+
+
+
+############# Versions of components
+
+variable "k8s_ver" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "k8s_version_short" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "calico_version" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "vsphere_csi_driver_version" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "k8s_controlPlane_Endpoint" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "k8s_service_subnet" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "k8s_pod_subnet" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "calico_network_cidr_blocksize" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "k8s_cluster_id" {
+  type        = string
+  default     = ""
+  description = "The unique cluster identifier. DISPLAYED IN VSPHERE CONSOLE(CONTAINER VOLUMES DETAILS)"
+ }
+variable "sc_storage_policy_name" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable "sc_name" {
+  type        = string
+  default     = ""
+  description = "description"
+ }
+variable vm_user_ssh_pk {
+  type        = string
+  default     = ""
+  description = "description"
 }
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
